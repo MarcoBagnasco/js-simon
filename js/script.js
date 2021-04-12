210ec05dc63fa9494b8c42a015615a10cb161ee6
MarcoBagnasco <bagna.hbk@gmail.com>
2021-04-12 17:03:17 +0200

    add alert

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
-    console.log($); //test
+    /****************************************************************
+     * SIMON SAYS
+       - Un alert() espone 5 numeri generati casualmente.
+       - Da li parte un timer di 30 secondi.
+       - Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+       - Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+    ****************************************************************/
 
     //End Doc Ready
 });
+    var numbers = [];
+    
+    //Generate five random numbers
+    while(numbers.length < 5){
+        var num = randNum();
+
+        if(!numbers.includes(num)){
+            numbers.push(num);
+        }
+    }
+    //Alert
+    alert('Remember these numbers\n' + numbers);
+
+
+
+    
+/* *************
+* FUNCTIONS
+************ */
+/**
+ * Returns a random number between 1 and 100
+ * @returns 
+ */
+function randNum(){
+    return Math.floor(Math.random() * 100 + 1);
+}
